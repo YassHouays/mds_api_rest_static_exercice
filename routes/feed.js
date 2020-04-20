eba68fd1af169538df9cd358e1cbfefbc0c7a760
HOUAYS Yassine <Yass76300@gmail.com>
2020-04-20 14:41:25 +0200

    request get post

--- routes/feed.js
+++ routes/feed.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 //POST /feed/post
-router.get('/post', feedController.createPost);
+router.post('/post', feedController.createPost);
 
 module.exports = router;
